33bd82b0790819290d05886b7ead7b6bd1fa52f4
coderLMN <jinzhaowu@yahoo.com>
2014-04-05 00:09:05 +0800

    downgrade Express and install Passport for OpenID auth
    Downgrade Express to 3.1.0, because under v3.5.1 the static file
    index.html is no longer rendered automatically for path '/', which is
    necessary for a single page app.

---
+++ public/js/front.js
@@ -0,0 +1,99 @@
+
+var app = angular.module('app', ['ngResource', 'ui.bootstrap']);
+console.log('front now take control');
+app.config(['$routeProvider', '$locationProvider', '$httpProvider',
+    function($routeProvider, $locationProvider, $httpProvider){
+        $routeProvider
+            .when('/',{
+                templateUrl: '/partial/authed.html',
+                controller: 'authedCtrl'
+            })
+            .when('/login',{
+                templateUrl: '/partial/login.html',
+                controller: 'LoginCtrl'
+            })
+////            .when('/quit/:id',{
+////                templateUrl: '/partial/quit.partial',
+////                controller: 'QuitCtrl'
+////            })
+//            .when('/404',
+//            {
+//                templateUrl:'/partial/404.html'
+//            })
+            .when('/401',
+            {
+                templateUrl:'/partial/login.html'
+            })
+            .otherwise({
+                redirectTo: function(routerParams, path){
+                    console.log('redirecting to ...' + path);
+                    console.log(routerParams);
+                    console.log(path);
+                    return path ;
+                }
+            });
+//        console.log($routeProvider);
+        $locationProvider.html5Mode(true);
+        var interceptor = ['$location', '$rootScope', '$q', function($location, $rootScope, $q) {
+            function success(response) {
+                return response;
+            }
+
+            function error(response) {
+                var status = response.status;
+                if (status === 401  && $location.path()!='/401') {
+                    $rootScope.savePath = $location.path();   //save current path for reloading after logged in
+                    console.log('>>>>>>>>>>>>>>>>>>>>');
+                    console.log($rootScope.savePath);
+                    $location.path('/401');
+//                    return;
+                }
+                else if (status === 403) {
+                    $location.path("/forbidden");
+//                    return;
+                }
+                else if (status === 404) {
+                    $location.path("/404");
+//                    return;
+                }
+                // otherwise
+
+                return $q.reject(response);
+
+            }
+
+            return function (promise) {
+                return promise.then(success, error);
+            }
+
+        }];
+        $httpProvider.responseInterceptors.push(interceptor);
+    }]
+)
+//
+.run(['$rootScope', '$location',  function ($rootScope, $location) {
+
+    $rootScope.$on("$routeChangeStart", function (event, next, current) {
+        console.log('===================== route change ==============');
+        console.log(event);
+        console.log(next);
+        console.log(current);
+        console.log('===================== end ==============');
+//        alert('change now');
+    });
+
+}]);
+
+app.controller('LoginCtrl', function($resource ,$scope) {
+    $scope.message = "请输入您的用户名和密码：";
+    $scope.login = function() {
+        console.log('login now');
+        $scope.message = $resource('/login').query();
+    };
+});
+
+app.controller('authedCtrl', function($scope, $resource){
+    console.log('authed ctroller');
+    $scope.test = $resource('/test').get();
+}) ;
+
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
+
 function authenticated(req, res, next) {
-    if(req.session.urole){
-        next();
-    }
-    else{
-//        res.send(401);
-        res.sendfile('public/login.html');
-    }
+    if (req.isAuthenticated()) { return next(); }
+    res.send(401);
 }
 
 var express = require('express')
@@ -16,46 +12,109 @@ var redis = require('redis');
 var db = redis.createClient();
 var app = express();
 
+
+var passport = require('passport')
+    , YahooStrategy = require('passport-yahoo').Strategy;
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new YahooStrategy({
+        returnURL: 'http://localhost:3000/auth/yahoo/return',
+        realm: 'http://localhost:3000/'
+    },
+    function(identifier, profile, done) {
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+
+            // To keep the example simple, the user's Yahoo profile is returned to
+            // represent the logged-in user.  In a typical application, you would want
+            // to associate the Yahoo account with a user record in your database,
+            // and return that user instead.
+            profile.identifier = identifier;
+            req.session.user = identifier.email;
+            console.log(identifier);
+            return done(null, profile);
+        });
+    }
+));
+
 app.configure(function(){
     app.set('port', process.env.PORT || 3000);
-    app.use(express.favicon());
+//    app.use(express.favicon());
     app.use(express.logger('dev'));
     app.use(express.json());
-    app.use(express.urlencoded());
-    app.use(express.methodOverride());
+//    app.use(express.static(path.join(__dirname, 'public')));
+//    app.use(express.urlencoded());
+//    app.use(express.methodOverride());
     app.use(express.cookieParser("LMN"));
-    app.use(express.session());
+    app.use(express.session({ secret: 'keyboard cat' }));
+    // Initialize Passport!  Also use passport.session() middleware, to support
+    // persistent login sessions (recommended).
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(express.compress());
     app.use(app.router);
+
     app.use(express.static(path.join(__dirname, 'public')));
-});
 
+});
+console.log(path.join(__dirname, 'public'));
 app.configure('development', function(){
-    app.use(express.errorHandler());
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 
-app.use(function(req, res, next){
-    var ua = req.headers['user-agent'];
-    db.zadd('online', Date.now(), ua, next);
-});
+//app.use(function(req, res, next){
+//    var ua = req.headers['user-agent'];
+//    db.zadd('online', Date.now(), ua, next);
+//});
+//
+//app.use(function(req, res, next){
+//    var min = 60 * 1000;
+//    var ago = Date.now() - min;       //user count in the recent minute
+//    db.zrevrangebyscore('online', '+inf', ago, function(err, users){
+//        if (err) return next(err);
+//        req.online = users;
+//        next();
+//    });
+//});
 
-app.use(function(req, res, next){
-    var min = 60 * 1000;
-    var ago = Date.now() - min;       //user count in the recent minute
-    db.zrevrangebyscore('online', '+inf', ago, function(err, users){
-        if (err) return next(err);
-        req.online = users;
-        next();
-    });
-});
 
 
-app.get('/', authenticated, function(req, res) {
-    res.sendfile('public/index.html');
+app.get('/test', authenticated, function(req, res) {
+    console.log('server side test');
+    res.end('<h1>authed!</h1>');
     // load the single view file (angular will handle the page changes on the front-end)
 });
 
+// GET /auth/yahoo
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Yahoo authentication will involve redirecting
+//   the user to yahoo.com.  After authenticating, Yahoo will redirect the
+//   user back to this application at /auth/yahoo/return
+app.get('/auth/yahoo',
+    passport.authenticate('yahoo', { failureRedirect: '/401' }),
+    function(req, res) {
+        res.redirect('/');
+    });
 
-var server = app.listen(3000, function() {
+// GET /auth/yahoo/return
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/yahoo/return',
+    passport.authenticate('yahoo', { failureRedirect: '/' }),
+    function(req, res) {
+        res.redirect('/');
+    });
+
+var server = app.listen(3003, function() {
     console.log('Listening on port %d', server.address().port);
 });
