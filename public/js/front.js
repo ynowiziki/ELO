df3884b431ab2b46ecdbeaef6f7ae8fd1e75ab69
coderLMN <jinzhaowu@yahoo.com>
2014-04-11 00:46:52 +0800

    refresh user info before edit it

--- public/js/front.js
+++ public/js/front.js
@@ -56,9 +56,9 @@ app.config(['$routeProvider', '$locationProvider', '$httpProvider',
 }]);
 
 app.controller('userCtrl', function($scope, $resource, $location, imageResizeService, $rootScope){
-//    $resource('/userInfo').get(function(user){
-//        $rootScope.user = user;
-//    });
+    $resource('/userInfo').get(function(user){
+        $rootScope.user = user;
+    });
 
     $scope.selectFile = function(element) {    //select image files within the photos directory
             var file = element.files[0];
